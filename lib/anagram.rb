2f5b54611bfb54d8ec1603c35b4c945f71c1a06b
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-13 11:56:35 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(some_words)
+    return_array = []
+    some_words.map do |array_word| # iterate over each word
+      # compare @word to the array_word
+      if @word.split("").sort == array_word.split("").sort
+        #return if equal
+        return_array << array_word
+      end
+    end
+    return_array
+  end
+
+end
+
+=begin
+
+  some_words = %w(@array)  # turn array in a CSV  
+
+  def array= (array)
+    @array = array
+  end
+
+=end
