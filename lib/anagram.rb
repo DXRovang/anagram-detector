55b1740ceb433ed76b67e04ce3fccffb6480095c
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-18 18:22:08 -0500

    commit tree 15b8436b01c0d4af4f7dbe5dc47a9253e51b9214

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,25 +9,9 @@ class Anagram
   end
 
   def match(some_words)
-    return_array = []
-    some_words.map do |array_word| # iterate over each word
-      # compare @word to the array_word
-      if @word.split("").sort == array_word.split("").sort
-        #return if equal
-        return_array << array_word
-      end
+    some_words.select do |array_word|   
+      @word.split("").sort == array_word.split("").sort 
     end
-    return_array
   end
 
 end
-
-=begin
-
-  some_words = %w(@array)  # turn array in a CSV  
-
-  def array= (array)
-    @array = array
-  end
-
-=end
